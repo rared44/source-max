c7cea078a1a3d12212ecd8f036a80f958edbb33e
ماكس <148282422+rared44@users.noreply.github.com>
2023-12-11 09:40:30 +0200

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -2,14 +2,23 @@ from sample_config import Config
 
 
 class Development(Config):
-    # ضـع اكـوادك تحت مكـان الكـلام العـربي  .. امسـح الكلام العربي اللي بين " " فقـط وضع اكـوادك
-    APP_ID = 6 #ضع كود الايبي ايدي الصغيـر مكان الرقم 6 نسخ لصق   
-    API_HASH = "ضع كود الايبي هاش"
-    ALIVE_NAME = "اسم حسابك التلي"
-    DB_URI = "رابـط التخـزين الخـاص بك"
-    STRING_SESSION = "كود تيرمــكس"
-    TG_BOT_TOKEN = "توكـن البـوت الخـاص بك"
-    PRIVATE_GROUP_BOT_API_ID = -100  # ضـع ايـدي كـروب السجـل بجـانب العـدد -100 لا تمسـح العـدد هـذا
-    COMMAND_HAND_LER = "."  # اتركهــا كمـا هـي
-    SUDO_COMMAND_HAND_LER = "."  # اتركهــا كمـا هـي
-    TZ = "Asia/Baghdad"  # اتركهــا كمـا هـي
+    # get this values from the my.telegram.org
+    APP_ID = 6
+    API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
+    # the name to display in your alive message
+    ALIVE_NAME = "Your value"
+    # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
+    DB_URI = "Your value"
+    # After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
+    STRING_SESSION = "Your value"
+    # create a new bot in @botfather and fill the following vales with bottoken and username respectively
+    TG_BOT_TOKEN = "Your value"
+    TG_BOT_USERNAME = "Your value"
+    # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
+    PRIVATE_GROUP_BOT_API_ID = -100
+    # command handler
+    COMMAND_HAND_LER = "."
+    # sudo enter the id of sudo users userid's in that array
+    SUDO_USERS = []
+    # command hanler for sudo
+    SUDO_COMMAND_HAND_LER = "."
